ce52c18f0f895d1c9f0f489cd8678190ba8a43f4
Sorin Marian <marian.sorin@yahoo.com>
2025-07-02 00:06:43 +0300

    Add new entity RateLimitEntry

--- src/Entity/RateLimitEntry.php
+++ src/Entity/RateLimitEntry.php
@@ -10,10 +10,6 @@ use Doctrine\ORM\Mapping as ORM;
 class RateLimitEntry
 {
     #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column]
-    private ?int $id = null;
-
     #[ORM\Column(length: 255)]
     private ?string $fieldKey = null;
 
